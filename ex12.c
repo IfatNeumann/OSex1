e31eb9bb1c60dff408e6f633f3e75c752b6cac81
ifat <ineumann19@gmail.com>
2017-05-02 14:55:31 +0300

    starting part B

---
+++ ex12.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+
+//160*3=480
+#define MAXSIZE 480
+int main(int argc, char *argv[]) {
+    //1-identical,2-similar,3-different
+    char* buffer[MAXSIZE], mainFolder, inputFile, outputFile;
+    char error[] = "Error!,System call failed.";
+    const char s[2] = "\n";
+    int readSize;
+    int fd = open(argv[1], O_RDONLY, S_IREAD);
+    if(fd == -1){
+        write(2,error,strlen(error));
+        return -1;
+    }
+    *readSize = read(fd1, buffer, SIZE);
+    mainFolder = strtok(buffer,s);
+    inputFile = strtok(NULL,s);
+    outputFile = strtok(NULL,s);
+
+}
